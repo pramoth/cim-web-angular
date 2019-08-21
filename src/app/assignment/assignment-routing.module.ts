bbe2c432a6360d03e8a1ac59b56a5e3b1dd109cd
Saranrat <saranrat@geniustree.co.th>
2019-08-21 16:11:40 +0700

    made detail page

--- src/app/assignment/assignment-routing.module.ts
+++ src/app/assignment/assignment-routing.module.ts
@@ -1,25 +1,27 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AllComponent} from "./all/all.component";
-
+import {DetailComponent} from "./detail/detail.component";
 
 
 const routes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'all'
-  },
-  {
-    path: 'all',
-    pathMatch: 'full',
-    component: AllComponent,
-    data: {name: 'รอมอบหมายงาน', header: 'รอมอบหมายงาน', iconClass: undefined}
-  },
+    {
+        path: '',
+        pathMatch: 'full',
+        component: AllComponent,
+        data: {name: 'รอมอบหมายงาน', header: 'รอมอบหมายงาน', iconClass: undefined}
+    }, {
+        path: 'detail',
+        pathMatch: 'full',
+        component: DetailComponent,
+        data: {name: 'มอบหมายงาน', header: 'มอบหมายงาน', iconClass: undefined}
+    }
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
-export class AssignmentRoutingModule { }
+export class AssignmentRoutingModule {
+}
--- src/app/assignment/assignment.module.ts
+++ src/app/assignment/assignment.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { AssignmentRoutingModule } from './assignment-routing.module';
-import { AllComponent } from './all/all.component';
-import {NgbPaginationModule} from "@ng-bootstrap/ng-bootstrap";
+import {AssignmentRoutingModule} from './assignment-routing.module';
+import {AllComponent} from './all/all.component';
+import {NgbPaginationModule, NgbTabsetModule} from "@ng-bootstrap/ng-bootstrap";
+import {DetailComponent} from './detail/detail.component';
 
 
 @NgModule({
-  declarations: [AllComponent],
-  imports: [
-    CommonModule,
-    AssignmentRoutingModule,
-    NgbPaginationModule
-  ]
+    declarations: [AllComponent, DetailComponent],
+    imports: [
+        CommonModule,
+        AssignmentRoutingModule,
+        NgbPaginationModule,
+        NgbTabsetModule
+    ]
 })
-export class AssignmentModule { }
+export class AssignmentModule {
+}
