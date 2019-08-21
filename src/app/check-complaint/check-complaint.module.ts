623e9ce49a0609adedffbc1df2a638171415e85f
Dop <narit@geniustree.co.th>
2019-08-21 16:50:07 +0700

    check-complaint detail forward

--- src/app/check-complaint/check-complaint.module.ts
+++ src/app/check-complaint/check-complaint.module.ts
@@ -6,10 +6,11 @@ import {CheckComplaintComponent} from './check-complaint.component';
 import {NgbPaginationModule, NgbTabsetModule} from '@ng-bootstrap/ng-bootstrap';
 import {DetailComponent} from './detail/detail.component';
 import { AboutComponent } from './detail/about/about.component';
+import { ForwardComponent } from './detail/forward/forward.component';
 
 
 @NgModule({
-    declarations: [CheckComplaintComponent, DetailComponent, AboutComponent],
+    declarations: [CheckComplaintComponent, DetailComponent, AboutComponent, ForwardComponent],
     imports: [
         CommonModule,
         CheckComplaintRoutingModule,
