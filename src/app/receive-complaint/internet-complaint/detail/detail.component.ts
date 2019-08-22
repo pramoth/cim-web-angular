39da3185aaf952817c44da5a470f3823296a2ae7
pondqwas <pondqwas@gmail.com>
2019-08-22 11:26:17 +0700

    add keyin component

---
+++ src/app/receive-complaint/internet-complaint/detail/detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-internet',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.scss']
+})
+export class InternetDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/receive-complaint/post-complaint/complainant-detail/complainant-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-complainant-detail-post',
+  templateUrl: './complainant-detail.component.html',
+  styleUrls: ['./complainant-detail.component.scss']
+})
+export class PostComplainantDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
