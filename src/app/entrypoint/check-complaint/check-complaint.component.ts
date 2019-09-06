8ea424d1020e7d06381632d25f003c42838e5d1f
Pramoth Suwanpech <pramoth@geniustree.co.th>
2019-09-07 00:03:32 +0700

    add action to search /advance search

--- src/app/entrypoint/check-complaint/check-complaint.component.ts
+++ src/app/entrypoint/check-complaint/check-complaint.component.ts
@@ -8,15 +8,16 @@ import {Component, OnInit} from '@angular/core';
 export class CheckComplaintComponent implements OnInit {
 
     page = 1;
-    items: { name: string, route: string ,complainant:string}[] = [
-        {name: 'Walk In', route: 'walk-in',complainant:'นายสมชาย ใจดี'},
-        {name: 'ไปรษณีย์', route: 'post',complainant:'นายจอห์น วิก'},
-        {name: 'Call Center', route: 'call-center',complainant:'นางสมหญิง ใจเด็ด'},
-        {name: 'Internet', route: 'internet',complainant:'นางสาวมะนาว โซดา'},
-        {name: 'Mobile', route: 'mobile',complainant:'นายรุ่งเรือง จิตใจดี'},
-        {name: 'ผผ.หยิบยก', route: 'own-motion',complainant:'นายเจ้าขุน ทอง'}];
+    items: { name: string, route: string, complainant: string }[] = [
+        {name: 'Walk In', route: 'walk-in', complainant: 'นายสมชาย ใจดี'},
+        {name: 'ไปรษณีย์', route: 'post', complainant: 'นายจอห์น วิก'},
+        {name: 'Call Center', route: 'call-center', complainant: 'นางสมหญิง ใจเด็ด'},
+        {name: 'Internet', route: 'internet', complainant: 'นางสาวมะนาว โซดา'},
+        {name: 'Mobile', route: 'mobile', complainant: 'นายรุ่งเรือง จิตใจดี'},
+        {name: 'ผผ.หยิบยก', route: 'own-motion', complainant: 'นายเจ้าขุน ทอง'}];
 
-    advanceSearch:boolean=false;
+    advanceSearch: boolean = false;
+    result: any;
 
     constructor() {
     }
