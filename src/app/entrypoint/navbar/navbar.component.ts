3e6c4a636063c568f394c6730c2c19612fefca77
Coco <chatchai@geniustree.co.th>
2019-09-06 11:12:28 +0700

    fix button

--- src/app/entrypoint/navbar/navbar.component.ts
+++ src/app/entrypoint/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+    @Output() toggleMenuSidebar: EventEmitter<any> = new EventEmitter<any>();
+    public searchForm: FormGroup;
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        this.searchForm = new FormGroup({
+            search: new FormControl(null)
+        });
+    }
 }
