49e62d8c2c95b9f9ef41b082c2c41bf0cab88d50
Coco <chatchai@geniustree.co.th>
2019-09-06 17:22:32 +0700

    include tab

--- src/app/entrypoint/check-complaint/check-complaint.module.ts
+++ src/app/entrypoint/check-complaint/check-complaint.module.ts
@@ -15,8 +15,6 @@ import {FactComponent} from './detail/fact/fact.component';
 import {AboutComponent} from './detail/about/about.component';
 import {ForwardComponent} from './detail/forward/forward.component';
 import {PrintComponent} from './detail/print/print.component';
-import {ComplaintInfoComponent} from './detail/complaint-info/complaint-info.component';
-import {ComplaintComponent} from './detail/complaint/complaint.component';
 import {ConcludeComponent} from './detail/conclude/conclude.component';
 import {AttachmentComponent} from './detail/attachment/attachment.component';
 import {ReceiveDocComponent} from './detail/receive-doc/receive-doc.component';
@@ -33,8 +31,6 @@ import {FormsModule} from "@angular/forms";
         ForwardComponent,
         PrintComponent,
         FactComponent,
-        ComplaintInfoComponent,
-        ComplaintComponent,
         AttachmentComponent,
         ReceiveDocComponent,
         PlanComponent,
