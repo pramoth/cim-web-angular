0ae2c9b570f583594df9624cbd6e6ac3b181ca66
Saranrat <saranrat@geniustree.co.th>
2019-09-08 15:02:44 +0700

    change table

--- src/app/entrypoint/check-complaint/detail/step/step.component.ts
+++ src/app/entrypoint/check-complaint/detail/step/step.component.ts
@@ -7,6 +7,111 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StepComponent implements OnInit {
 
+  data: { date: string, completedate: string, amountdate: string, step: string, operator: string }[] = [{
+    date: "31/05/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "รับเรื่องร้องเรียน",
+    operator: "นางสาวรศิพร เสวกพันธ์"
+  }, {
+    date: "04/06/2561",
+    completedate: "04/06/2561",
+    amountdate: "",
+    step: "ส่งสารรับผิดชอบ",
+    operator: "นายทิฆมพร ยะลา"
+  }, {
+    date: "12/06/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "มอบหมายรับผิดชอบ",
+    operator: "นายทิฆมพร ยะลา"
+  }, {
+    date: "12/06/2561",
+    completedate: "12/06/2561",
+    amountdate: "",
+    step: "ส่งสำนักงานรับผิดชอบ",
+    operator: "นายทิฆมพร ยะลา"
+  }, {
+    date: "12/06/2561",
+    completedate: "12/06/2561",
+    amountdate: "",
+    step: "ส่งสำนักงานรับผิดชอบ",
+    operator: "นายทิฆมพร ยะลา"
+  }, {
+    date: "18/06/2561",
+    completedate: "18/06/2561",
+    amountdate: "",
+    step: "ส่งสำนักงานรับผิดชอบ",
+    operator: "นายทิฆมพร ยะลา"
+  }, {
+    date: "06/07/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "มอบหมายผู็รับผิดชอบ",
+    operator: "นายกีรป กฤตธีรานนท์"
+  }, {
+    date: "06/07/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "แจ้งความคืบหน้า",
+    operator: "นางสาวรุ่งทิพย์ ปาระมีแจ้"
+  }, {
+    date: "06/07/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "ให้หน่วยงานชี้แจง",
+    operator: "นางสาวรุ่งทิพย์ ปาระมีแจ้"
+  }, {
+    date: "11/07/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "ให้หน่วยงานชี้แจง",
+    operator: "นางสาวรุ่งทิพย์ ปาระมีแจ้"
+  }, {
+    date: "18/07/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "ได้รับหนังสือเพิ่ม",
+    operator: "นายฐนภณ ธนวชิรนนท์"
+  }, {
+    date: "11/07/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "ได้รับหนังสือเพิ่ม",
+    operator: "นายฐนภณ ธนวชิรนนท์์"
+  }, {
+    date: "18/07/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "ได้รับหนังสือเพิ่ม",
+    operator: "นายฐนภณ ธนวชิรนนท์"
+  }, {
+    date: "23/07/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "ได้รับหนังสือเพิ่ม",
+    operator: "นายฐนภณ ธนวชิรนนท์"
+  }, {
+    date: "10/10/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "เสนอผู้ตรวจการแผนดิน",
+    operator: "นางสาวพีรัญญา โพธิเสถียร"
+  }, {
+    date: "30/10/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "ยุติเรื่อง/แจ้งผลการวินิจฉัย",
+    operator: "นายเฉลิมพล ขุนเมือง"
+  }, {
+    date: "02/11/2561",
+    completedate: " ",
+    amountdate: "",
+    step: "ยุติเรื่อง/แจ้งผลการวินิจฉัย",
+    operator: "นางสาวจุฑารัตน์ สุขสละ"
+  }
+  ];
+
   constructor() { }
 
   ngOnInit() {
