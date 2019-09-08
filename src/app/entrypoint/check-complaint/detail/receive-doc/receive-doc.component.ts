0d93e8d65ec470db4cbd6394855c012048e7009f
Coco <chatchai@geniustree.co.th>
2019-09-08 12:54:59 +0700

    update follow-status

--- src/app/entrypoint/check-complaint/detail/receive-doc/receive-doc.component.ts
+++ src/app/entrypoint/check-complaint/detail/receive-doc/receive-doc.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./receive-doc.component.scss']
 })
 export class ReceiveDocComponent implements OnInit {
+  data: number[];
 
   constructor() { }
 
   ngOnInit() {
-  }
+    this.data= [1,2,3,4,5,6,7]
 
+  }
+  deleteRow(i) {
+    const index = this.data.indexOf(this.data.length);
+    this.data.splice(index, 1);
+  }
 }
