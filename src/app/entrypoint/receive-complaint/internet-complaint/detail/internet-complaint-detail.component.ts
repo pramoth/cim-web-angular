9a8e0f7ecbbcbe5e3511a9bc4670cdd50633c397
pondqwas <pondqwas@gmail.com>
2019-09-08 14:33:55 +0700

    edit receive complaint component

--- src/app/entrypoint/receive-complaint/internet-complaint/detail/internet-complaint-detail.component.ts
+++ src/app/entrypoint/receive-complaint/internet-complaint/detail/internet-complaint-detail.component.ts
@@ -11,9 +11,39 @@ export class InternetComplaintDetailComponent implements OnInit {
 
   complainantType: 'thai' | 'foreign' | 'agent' | 'group';
 
+  complainants: any[] = [{complainantType: null, subComplainants: [{}]}];
+
+  subComplainants: any[];
+
+  accusedDepartments: any[] = [{}];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addComplainant() {
+    this.complainants.push({complainantType: null, subComplainants: [{}]});
+  }
+
+  deleteComplainant() {
+    this.complainants.pop();
+  }
+
+  addSubComplainant(complainant: any) {
+    complainant.subComplainants.push({});
+  }
+
+  deleteSubComplainant(complainant: any) {
+    complainant.subComplainants.pop();
+  }
+
+  addDepartment() {
+    this.accusedDepartments.push({});
+  }
+
+  deleteDepartment() {
+    this.accusedDepartments.pop();
   }
 
 }
