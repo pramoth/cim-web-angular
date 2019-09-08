2f88e2b08b0f3775beb54b832a517c8cf8e37214
Coco <chatchai@geniustree.co.th>
2019-09-08 16:25:09 +0700

    update page follow-status

--- src/app/entrypoint/check-complaint/detail/attachment/attachment.component.ts
+++ src/app/entrypoint/check-complaint/detail/attachment/attachment.component.ts
@@ -7,7 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AttachmentComponent implements OnInit {
 
-    data: { id: number }[] = [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}, {id: 6}, {id: 7}, {id: 8}, {id: 9}, {id: 10}, {id: 11}];
+    data: { id: number }[] = [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5},
+        {id: 6}, {id: 7}, {id: 8}, {id: 9}, {id: 10}, {id: 11}];
 
 
     constructor() {
